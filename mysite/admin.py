e75bef1087012c49b13ea75b23f27f07a0682ee3
sk1227071686 <1227071686@qq.com>
2019-10-28 18:56:51 -0700

    first commit

---
+++ mysite/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from mysite import models
+
+admin.site.register(models.Maker)
+admin.site.register(models.PModel)
+admin.site.register(models.PPhoto)
+
+# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+	list_display=('pmodel','nickname','price','year')
+	search_fields=('nickname',)
+	ordering = ('-price',)
+
+admin.site.register(models.Product,ProductAdmin)
